68f9cb63c832b78105be66843cb832c79b67b6ba
Quodnon <feanorfeanturi@gmail.com>
2017-12-14 00:39:50 +0200

    master detail component + generated plain service

--- src/app/heroes-list.ts
+++ src/app/heroes-list.ts
@@ -2,16 +2,16 @@ import Hero from './hero';
 
 export const heroes: Hero[] = [
     {
-        id: 1, name: 'Kelnmiir',
+        id: 2, name: 'Kelnmiir',
         description: 'archmage Inventor, curiosity, technology',
-        imgUrl: 'aaa.jpg',
+        imgUrl: 'image.png',
         exp: 0,
         level: 1
     },
     {
-        id: 2, name: 'Quodnon',
+        id: 1, name: 'Quodnon',
         description: 'Leadership, responsibility, self discipline and willpower',
-        imgUrl: 'aaa.jpg',
+        imgUrl: 'image(1).png',
         exp: 0,
         level: 1
     },
@@ -19,10 +19,10 @@ export const heroes: Hero[] = [
         id: 3,
         name: 'Feanturi',
         description: 'Healer, communicator, keeper of health and good feeling',
-        imgUrl: 'aaa.jpg',
+        imgUrl: 'image(2).png',
         exp: 0,
         level: 1
     },
-    { id: 4, name: 'Jinx', description: 'bard, trixter, joker, trouble maker', imgUrl: 'aaa.jpg', exp: 0, level: 1 },
+    { id: 4, name: 'Jinx', description: 'bard, trixter, joker, trouble maker', imgUrl: 'jinx.jpg', exp: 0, level: 1 },
     { id: 5, name: 'Rom', description: 'vacuous spider', imgUrl: 'aaa.jpg', exp: 0, level: 1 },
 ];
