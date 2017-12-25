75924e8d30f49439a0953485b2360fa9cd9c4ab2
Quodnon <feanorfeanturi@gmail.com>
2017-12-26 00:40:35 +0200

    Before adding web-api

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,11 @@ export class HeroService {
     return of(heroes);
     // here should be fetch to firebase for list of heroes;
   }
+  getHero(id: number): Observable<Hero> {
+    // Todo: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(heroes.find(hero => hero.id === id));
+  }
   constructor(private messageService: MessageService) { }
 
 }
